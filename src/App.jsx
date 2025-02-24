96a26ada41c621392c055cc9fb019d6733345847
mynameist04st3r <t04st3r@mac.com>
2025-02-24 14:44:01 -0500

    Set up left side NavBar - Incomplete.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './NavBar';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+
+
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <header className='header'>Pokedex</header>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="nav-and-options-container">
+        <NavBar/>
+        <div className="main-content">
+          {/* Main Page Stuff */}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+
+    </div>
   )
 }
 
 export default App
+
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
---
+++ src/NavBar.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+
+const NavBar = () => {
+  const [selectOptions, setSelectOptions] = useState({});
+  const handleCheckboxChange = (e) => {
+    const {name, checked} = e.target;
+    setSelectOptions((onOption) => ({ ...onOption, [name]: checked}));
+  };
+  return (
+
+    <div className="nav-and-options-container">
+      <div className="nav-container">
+        <h3>Links</h3>
+        <div>
+          <a href="">Home</a>
+        </div>
+        <div>
+          <a href="">About</a>
+        </div>
+      </div>
+      <div className="options-container">
+        <ul>
+          <li>
+            <input
+              type="checkbox"
+              name="option1"
+              checked={selectOptions.option1}
+              onChange={handleCheckboxChange}
+              /> Option 1
+          </li>
+          <li>
+            <input
+              type="checkbox"
+              name="option1"
+              checked={selectOptions.option2}
+              onChange={handleCheckboxChange}
+              /> Option 2
+          </li>
+          <li>
+            <input
+              type="checkbox"
+              name="option1"
+              checked={selectOptions.option3}
+              onChange={handleCheckboxChange}
+              /> Option 3
+          </li>
+        </ul>
+      </div>
+    </div>
+
+  )
+}
+
+export default NavBar;
